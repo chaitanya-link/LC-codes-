87e3335702223184597401cc0e4059abe9cb4d73
Chaitanya Joshi <chaitu11106@gmail.com>
2026-05-18 21:12:42 +0530

    Add files via upload

---
+++ q1431.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> final;
+
+        int m = *max_element(candies.begin(), candies.end());
+
+        for(int i = 0; i < candies.size(); i++) {
+            if(candies[i] + extraCandies >= m) {
+                final.push_back(true);
+            }
+            else {
+                final.push_back(false);
+            }
+        }
+
+        return final;
+    }
+};
