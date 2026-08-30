cf3549ffe278a62ca832c9c771197b5278d61b9a
chaitanya <chaitu11106@gmail.com>
2026-08-30 19:39:28 +0530

    sorting colors

---
+++ q75.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int nums[n];
+
+    cout << "Enter the digits (0, 1, 2): ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    // Sorting
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] > nums[j]) {
+                swap(nums[i], nums[j]);
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
