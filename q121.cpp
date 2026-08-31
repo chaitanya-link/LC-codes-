4985750e90d996c08b778655319cb84cdf32192a
chaitanya <chaitu11106@gmail.com>
2026-08-31 19:02:22 +0530

    best time to buy and sell a stock

---
+++ q121.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxP = 0;
+        int n=prices.size();
+        for(int i = 1; i < n; i++) {
+                if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            int profit = prices[i] - minPrice;
+             maxP = max(maxP, profit);
+        }
+         return maxP;
+    }
+};
